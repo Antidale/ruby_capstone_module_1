9023fe2ace459504a9c5377d4e2acdf4cf063097
Scott Wyngarden <swyngarden@gmail.com>
2018-06-27 11:02:05 -0700

    added states with mongoid

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   scope :api, defaults: {format: :json} do
     resources :cities, except: [:new, :edit, :update, :destroy, :create]
+    resources :states, except: [:new, :edit, :update, :destroy, :create]
   end
+
 end
