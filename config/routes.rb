e7bee11a39aed61963a8749383074b255bd15443
Scott Wyngarden <swyngarden@gmail.com>
2018-06-27 13:34:16 -0700

    production site should only be under construction

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  scope :api, defaults: {format: :json} do
-    resources :cities, except: [:new, :edit, :update, :destroy, :create]
-    resources :states, except: [:new, :edit, :update, :destroy, :create]
-  end
+  # scope :api, defaults: {format: :json} do
+  #   resources :cities, except: [:new, :edit, :update, :destroy, :create]
+  #   resources :states, except: [:new, :edit, :update, :destroy, :create]
+  # end
 
   get '/ui' => "ui#index"
   get '/ui#' => "ui#index"
